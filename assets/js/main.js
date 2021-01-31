780cd141b2d121007dfa05957cac918a22bcd9d4
Ganesh Naykawadi <ganeshnaykawadi@gmail.com>
2021-01-31 15:11:06 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,100 @@
+/*
+// Project Name: Search Pathalogy Labs.
+// Date Created: 31 Jan, 2021.
+// Author: Ganesh B. Naykawadi.
+*/
 
+
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    /*Active link*/
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    /*Remove menu mobile*/
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 1200,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.home__title', {});
+sr.reveal('.button', {
+    delay: 200
+});
+sr.reveal('.home__img', {
+    delay: 400
+});
+sr.reveal('.home__social-icon', {
+    interval: 200
+});
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img', {});
+sr.reveal('.about__subtitle', {
+    delay: 400
+});
+sr.reveal('.about__text', {
+    delay: 400
+});
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle', {});
+sr.reveal('.skills__text', {});
+sr.reveal('.skills__data', {
+    interval: 200
+});
+
+sr.reveal('.skills__img', {
+    interval: 600
+});
+
+/*SCROLL WORK*/
+sr.reveal('.card', {
+    interval: 300
+});
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input', {
+    interval: 200
+});
+
+// Image carousel/Slide-show effect
+var myIndex = 0;
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slide-show1");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+}
