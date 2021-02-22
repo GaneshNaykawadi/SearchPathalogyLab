ad7258b380aac28af0090fece8ede35235496cb2
Ganesh Naykawadi <ganeshnaykawadi@gmail.com>
2021-02-22 11:10:38 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,13 +88,70 @@ sr.reveal('.contact__input', {
 var myIndex = 0;
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("img-slide-show");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 3000); // Change image every 3 seconds.
+    var i;
+    var x = document.getElementsByClassName("img-slide-show");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000); // Change image every 3 seconds.
+}
+
+// Booking Form
+
+var my_modal = document.getElementById('ModelBooking');
+
+function displayModal() {
+    my_modal.style.display = 'block';
+}
+
+function hideModal() {
+    my_modal.style.display = 'none';
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == my_modal) {
+        my_modal.style.display = "none";
+    }
+}
+
+
+// SMTP JS - sending email
+
+function sendEmail() {
+    debugger;
+    let from = "searchdiagnostic.collection@gmail.com";
+    let subject = "New Collection Booking Request from: " + document.getElementById("fullname").value + " / (" + document.getElementById("mobile").value + ").";
+    let message = document.getElementById("details");
+
+    if (from != null && subject != null && message != null) {
+        debugger;
+        Email.send({
+            Host: "smtp.elasticemail.com",
+            Username: "searchdiagnostic.collection@gmail.com",
+            Password: "13986A5FB0E049F44CD8B24DA923057C21EB",
+            To: 'ganeshnaykawadi@gmail.com',
+            //To: 'yogeshdake123@gmail.com',
+            From: from,
+            Subject: subject,
+            Body: message.value
+        }).then(function (response) {
+
+            if (response == "OK")
+                $.notify("Thanks! Your response has been recorded.", "success");
+            else
+                $.notify("Something went wrong!", "error");
+        });
+    }
+
+    /*===== CLEAR THE FORM FIELDS =====*/
+    document.getElementById('fullname').value = '';
+    document.getElementById('mobile').value = '';
+    document.getElementById('details').value = '';
+    hideModal();
 }
